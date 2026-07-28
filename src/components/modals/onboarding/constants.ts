6e2d37e8ed89b36f1ddbeda436c315e3d0ca10e1
chaemin <cheamin0038@gmail.com>
2026-07-28 22:01:22 +0900

    [refactor/PS-36] 온보딩 step 별 타이틀&설명 설정

--- src/components/modals/onboarding/constants.ts
+++ src/components/modals/onboarding/constants.ts
@@ -4,3 +4,14 @@ export const TASK_OPTIONS = ["PPT", "레포트", "이메일", "보고서", "회�
 export const MAX_JOBS = 3;
 export const MAX_TASKS = 3;
 export const NICKNAME_MAX = 10;
+
+export const STEP_META = {
+  1: {
+    title: "프로필 설정",
+    description: "닉네임과 프로필 사진을 설정해주세요.",
+  },
+  2: {
+    title: "관심 직군 & 태스크",
+    description: "", // TODO: 문구 미정
+  },
+} as const;
