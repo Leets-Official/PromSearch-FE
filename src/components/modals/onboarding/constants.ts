fa847fcb05f830e3a09c669bab52577b1e0fa9e5
chaemin <cheamin0038@gmail.com>
2026-07-28 21:21:14 +0900

    [feat/PS-36] 온보딩 옵션·상수 정의 추가

---
+++ src/components/modals/onboarding/constants.ts
@@ -0,0 +1,6 @@
+export const JOB_OPTIONS = ["학생", "직장인", "자영업자", "기획자", "디자이너", "개발자"] as const;
+export const TASK_OPTIONS = ["PPT", "레포트", "이메일", "보고서", "회의록", "이미지 생성"] as const;
+
+export const MAX_JOBS = 3;
+export const MAX_TASKS = 3;
+export const NICKNAME_MAX = 10;
